ea0ea3d633a1a3fe9e32a1fd94021b39ba80b391
Nalin Jayasinghe <jayasinghe.nc@gmail.com>
2021-01-23 22:32:22 +0530

    improve length randomization

--- src/services/pythonApi/app.py
+++ src/services/pythonApi/app.py
@@ -12,6 +12,7 @@ file_path = 'files/random.txt'
 file_max_size = 1024 * 1024 * 2  # 2MB
 letters = string.ascii_lowercase
 str_max_length = 50
+integer_max_size = len(str(sys.maxsize))
 
 
 class Report(Resource):
@@ -21,14 +22,11 @@ class Report(Resource):
         with open(file_path, 'w') as file:
             file.write(content)
 
-        print(str(file_max_size))
-        print(len(content))
-
         return {'link': 'http://127.0.0.1:5000/report'}
 
     def get(self):
-        f = open(file_path, "r")
-        return get_content_summary(f.read())
+        with open(file_path, "r") as file:
+            return get_content_summary(file.read())
 
 
 class Download(Resource):
@@ -42,22 +40,22 @@ api.add_resource(Download, '/download')
 
 def get_content():
     content_size = 0
-    randomList = []
+    content = ''
     separator = ','
     while content_size < file_max_size:
         available_size = file_max_size - content_size
         random_object = get_random_object(available_size)
-        randomList.append(random_object)
+        content += separator + random_object
         content_size += len(random_object) + len(separator)
 
-    return separator.join(map(str, randomList))
+    return content
 
 
 def get_random_object(available_size):
     object_type = random.randrange(4)
     if object_type == 0:
         return get_random_alphabetical_strings(available_size)
-    elif object_type == 1:
+    elif object_type == 1 and available_size > 3:
         return get_random_real_numbers(available_size)
     elif object_type == 2:
         return get_random_integers(available_size)
@@ -73,19 +71,22 @@ def get_random_alphabetical_strings(available_size):
 
 
 def get_random_real_numbers(available_size):
-    max_length = len(str(sys.maxsize)) if available_size > len(
-        str(sys.maxsize)) else available_size
-    max_value = ((10**max_length)-1 / 100)
-    real_number = str(random.uniform(0, max_value))
-    result = real_number[:75] if len(real_number) > max_value else real_number
+    max_length = integer_max_size if available_size > integer_max_size else available_size
+    max_length = random.randint(3, max_length+1)
+    sub_length = int(max_length/2)
+    max_value = (10**sub_length)-1
+    real_number = str(random.randint(0, max_value) + random.random())
+    result = real_number[:max_length] if len(
+        real_number) > max_length else real_number
     return result
 
 
 def get_random_integers(available_size):
-    max_length = len(str(sys.maxsize)) if available_size > len(
-        str(sys.maxsize)) else available_size
+    max_length = integer_max_size if available_size > integer_max_size else available_size
+    max_length = random.randint(1, max_length+1)
     max_value = (10**max_length)-1
-    return str(random.randrange(0, max_value))
+    result = str(random.randint(0, max_value))
+    return result
 
 
 def get_random_alphanumerics(available_size):
